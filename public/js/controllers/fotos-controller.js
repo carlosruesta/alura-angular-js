8a8d44c32842ab505648dd1179bb42a2078902de
Carlos Ruesta <carlos@dcide.com.br>
2020-09-16 06:24:57 -0300

    Aula 02 - Uso de $http e ng-repeat + a diretiva ng-repeat + injeção de dependências baseada no nome de parâmetro + o serviço $http + o conceito de promise + comunicação com o back-end

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,9 +1,17 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
-	$scope.foto = {
-		titulo : 'Leão',
-		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	};
+	$scope.fotos = [];
+
+	$http.get('/v1/fotos')
+	.success(function(retorno) {
+		console.log(retorno);
+		$scope.fotos = retorno;
+	})
+	.error(function(erro) {
+		console.log(erro);
+	});
+
+
 
 
 });
