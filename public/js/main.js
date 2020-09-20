ee69df33ba27c32e220967b2210b7946d092880f
Carlos Ruesta <carlos@dcide.com.br>
2020-09-20 07:43:59 -0300

    Aula 08 - Exclusao de fotos
    - Para exclusao foi adicionado um botão com ng-click informando o método que tratará a delecao
      -> apresenta mensagem de sucesso ou erro na tela principal.html
      -> para evitar chamar a listagem do backend, o item excluido será extraido da lista via o slice do JS puro

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,11 @@ angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
 			controller: 'FotoController'
 		});
 
+		$routeProvider.when('/fotos/edit/:id', {
+			templateUrl: 'partials/foto.html',
+			controller: 'FotoController'
+		});
+
 		$routeProvider.otherwise({
 			redirectTo: '/fotos'
 		});
