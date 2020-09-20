3219661b7ebce22afa534d60cf7b7573cdc068eb
Carlos Ruesta <carlos@dcide.com.br>
2020-09-20 06:15:01 -0300

    Aula 07 - Adicionando um formulário para cadastro de fotos
    - No principal foi adicionado um botão novo que encaminha o usuário para o formulário de nova foto
    - Um partial foto.html com uma tag formulario e os input e controles necessarios;
    - No partial todos os elementos estão sendo controlados por directivas do angular: ng-model, ng-submit, angular-expression
    - Um controller novo para tratamento do formulario de cadastro;
    - Nesse controller contem um metodo submeter que será quem envie os dados para o servidor e a variable $scope que cuida dos dados do formulario

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
 
 		$routeProvider.when('/fotos/new', {
 			templateUrl: 'partials/foto.html',
-			controller: ''
+			controller: 'FotoController'
 		});
 
 		$routeProvider.otherwise({
