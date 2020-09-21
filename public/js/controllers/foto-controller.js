30c14ffb0c5f1423fba8a8c0c0891bafea79ca26
Carlos Ruesta <carlos@dcide.com.br>
2020-09-20 22:27:36 -0300

    Bonus 1 - BOnus - Adicionando no controllers as variaveis do scopy
    Signed-off-by: Carlos Ruesta <carlos@dcide.com.br>

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,37 +1,40 @@
-angular.module('alurapic').controller('FotoController', function($scope, $routeParams, recursoFoto, cadastroDeFotos) {
+angular.module('alurapic').controller('FotoController', ['$scope', 'recursoFoto', '$routeParams', 'cadastroDeFotos',
 
-	$scope.foto = {};
-	$scope.mensagem = '';
+	function($scope, recursoFoto, $routeParams, cadastroDeFotos) {
 
-	if (angular.isDefined($routeParams.fotoId)) {
-		recursoFoto.get({fotoId: $routeParams.fotoId},
-			function(foto) {
-				$scope.foto = foto;
-			},
-			function(erro) {
-				console.log(erro);
-				$scope.mensagem = 'Não foi possível obter a foto'
-			});
-	}
+		$scope.foto = {};
+		$scope.mensagem = '';
 
-	$scope.submeter = function() {
-
-		if (!$scope.formulario.$valid) {
-			return;
-		}
-		if ($scope.formulario.$valid) {
-			cadastroDeFotos.cadastrar($scope.foto)
-				.then(function(dados) {
-					$scope.mensagem = dados.mensagem;
-					if (dados.inclusao) {
-						$scope.foto = {};
-						$scope.formulario.$setPristine();
-					}
-					$scope.focado = true;
-				})
-				.catch(function(erro) {
-					$scope.mensagem = erro.mensagem;
+		if (angular.isDefined($routeParams.fotoId)) {
+			recursoFoto.get({fotoId: $routeParams.fotoId},
+				function(foto) {
+					$scope.foto = foto;
+				},
+				function(erro) {
+					console.log(erro);
+					$scope.mensagem = 'Não foi possível obter a foto'
 				});
 		}
-	};
-});
+
+		$scope.submeter = function() {
+
+			if (!$scope.formulario.$valid) {
+				return;
+			}
+			if ($scope.formulario.$valid) {
+				cadastroDeFotos.cadastrar($scope.foto)
+					.then(function(dados) {
+						$scope.mensagem = dados.mensagem;
+						if (dados.inclusao) {
+							$scope.foto = {};
+							$scope.formulario.$setPristine();
+						}
+						$scope.focado = true;
+					})
+					.catch(function(erro) {
+						$scope.mensagem = erro.mensagem;
+					});
+			}
+		};
+	}
+]);
